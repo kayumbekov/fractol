2ca033bfd8e5213d16653453e68ed2a9888c7913
kbekov <kbekov@student.42heilbronn.de>
2024-09-23 05:27:36 +0200

    check

--- fractol.h
+++ fractol.h
@@ -1,12 +1,31 @@
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-# include <stido.h> //TODO debugging
+# include <stdio.h> //TODO debugging
 # include <stdlib.h> //malloc free
 # include <unistd.h> //write
 # include <math.h>
 # include "minilibx-linux/mlx.h"
 
 
-#define ERROR_MESSAGE "Please enter \n\t\"./fractol mandelbrot\" or \n\t\"./fractol julia <value_1> <value_2>\"
+#define ERROR_MESSAGE "Please enter \n\t\"./fractol mandelbrot\" or \n\t\"./fractol julia <value_1> <value_2>\"\n"
 
+
+
+
+
+
+
+
+
+
+
+
+//Strings utils ***
+
+int ft_strncmp(char *s1, char *s2, int n);
+void	putstr_fd(char *s, int fd);
+
+
+
+#endif
--- string_utils.c
+++ string_utils.c
@@ -4,8 +4,29 @@
 
 int ft_strncmp(char *s1, char *s2, int n)
 {
-    if(NULL == s1 || NULL s2 || n < = 0)
+    if(NULL == s1 || NULL == s2 || n <= 0)
         return (0);
-    while(*s1 == *s2 && n> 0)
+    while(*s1 == *s2 && n > 0 && *s1 != '\0')
+	{
+		s1++;
+		s2++;
+		n--;
 
+	}
+	//char arre 1 byte int
+	//when 0, strings are equal !!
+	return (*s1 - *s2);
+}
+
+//Recursion
+
+void	putstr_fd(char *s, int fd)
+{
+	if(NULL == s || fd < 0)
+		return;
+	if(*s != '\0')
+	{
+		write(fd, s, 1);
+		putstr_fd(s + 1, fd);
+	}
 }
